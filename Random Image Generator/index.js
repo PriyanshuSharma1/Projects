fb44bdf44cdb07279e30f0eefe32f4f475aa6625
PriyanshuSharma1 <psneb420@gmail.com>
2023-01-10 22:36:40 +0545

    can generate multiple images at a time

--- Random Image Generator/index.js
+++ Random Image Generator/index.js
@@ -1,10 +1,14 @@
 const ImgContainerEl= document.querySelector(".image-container");
 const btnEl=document.querySelector(".btn");
 btnEl.addEventListener("click",()=>{
+    numImg=5;
 addNewImg();
 })
 function addNewImg(){
-const newImg=document.createElement("img");
+for (let index = 0; index < numImg; index++) {
+     const newImg=document.createElement("img");
 newImg.src=`https://picsum.photos/300?random=${Math.floor(Math.random()*200)}`
 ImgContainerEl.appendChild(newImg);
+    }
+
 }
