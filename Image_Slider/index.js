25a62863548afb15ec0bf989c0243dec611ce786
PriyanshuSharma1 <psneb420@gmail.com>
2023-01-13 23:14:33 +0545

    The prev button works

--- Image_Slider/index.js
+++ Image_Slider/index.js
@@ -1,5 +1,7 @@
 const nextEl=document.querySelector(".next");
-let currentImage=0;
+const prevEl=document.querySelector(".prev");
+
+let currentImage=1;
 const imgsEl=document.querySelectorAll("img");
 const imageContainerEl= document.querySelector(".image_container")
 nextEl.addEventListener("click",()=>
@@ -8,10 +10,17 @@ nextEl.addEventListener("click",()=>
     updateImg();
 
 })
+prevEl.addEventListener("click",()=>{
+    currentImage--;
+    updateImg();
+})
 function updateImg(){
     if(currentImage>= imgsEl.length)
     {
         currentImage=1;
     }
-imageContainerEl.style.transform=`translateX(-${currentImage*500}px)`
+    else if(currentImage<1){
+        currentImage=imgsEl.length;
+    }
+imageContainerEl.style.transform=`translateX(-${(currentImage-1)*500}px)`
 }
