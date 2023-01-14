4b5f5f793d59549d339fa95c4dcfa68e80fb4fc3
PriyanshuSharma1 <psneb420@gmail.com>
2023-01-14 22:50:04 +0545

    Functionality part completed

--- DarkModeToggle/index.js
+++ DarkModeToggle/index.js
@@ -1,1 +1,18 @@
-s
+const inputEl=document.querySelector(".input");
+const  bodyEl=document.querySelector("body");
+updateBody();
+function updateBody()
+{
+    if(inputEl.checked){
+        bodyEl.style.background="white";
+
+    }
+    else{
+        bodyEl.style.background="orange";
+
+    }
+}
+inputEl.addEventListener("input",()=>
+{
+    updateBody();
+})
