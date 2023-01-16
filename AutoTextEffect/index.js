177f059c1d6ad0b51348f8d88791140971d864c0
PriyanshuSharma1 <psneb420@gmail.com>
2023-01-16 23:38:35 +0545

    Functionality added to the project

---
+++ AutoTextEffect/index.js
@@ -0,0 +1,23 @@
+const containerEl= document.querySelector(".container");
+const careers=["Youtuber","Dancer","Developer","Writer","Singer"]
+let careerIndex=0;
+let characterIndex=0;
+containerEl.innerHTML= `
+<h1>
+            I am a Data Science Enthusiast.
+        </h1>
+`
+updateText()
+function updateText(){
+    characterIndex++;
+
+    containerEl.innerHTML= `
+    <h1> I am a ${careers[careerIndex].slice(0,characterIndex)}</h1>`
+    if(characterIndex==careers[careerIndex].length)
+    {
+        characterIndex=0;
+        careerIndex++;
+
+    }
+    setTimeout(updateText,400)
+}
